5da59f8bee0224dda6646042cb6b4a8355ee70de
enumura1 <akirayuto0718@gmail.com>
2025-01-16 19:57:05 +0900

    add:options

---
+++ front/roulette-app/src/constants/default-options.js
@@ -0,0 +1,12 @@
+
+export const DEFAULT_OPTIONS = [
+    { color: '#FF3B3B', name: 'Red', weight: 1, isWinning: false },
+    { color: '#00C9A7', name: 'Mint', weight: 1, isWinning: false },
+    { color: '#4D7FFF', name: 'Blue', weight: 1, isWinning: true },
+    { color: '#00B894', name: 'Green', weight: 1, isWinning: false },
+    { color: '#9B51E0', name: 'Purple', weight: 1, isWinning: false },
+    { color: '#FF66B2', name: 'Pink', weight: 1, isWinning: true },
+    { color: '#FFD700', name: 'Yellow', weight: 1, isWinning: false },
+    { color: '#FF9933', name: 'Orange', weight: 1, isWinning: false }
+  ];
+
